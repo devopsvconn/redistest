0142ac233723136f0f82a5aa10736d160f5dfd9d
Muhammad Farjad <Muhammad.Farjad@SHMA.COM>
2024-02-16 15:15:34 +0500

    redis password added python code modified

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import redis
-import base64
-from kubernetes import client, config
-
-config.load_kube_config()
-
-v1 = client.CoreV1Api()
-
-secret = v1.read_namespaced_secret("redis", "dmsng")
-password = base64.b64decode(secret.data['password']).decode()
 
 # Connect to Redis
-r = redis.Redis(host='redis-headless.dmsng.svc.cluster.local', port=6379, password=password)
+r = redis.Redis(host='redis-headless.dmsng.svc.cluster.local', port=6379, password='UCFl1FUSGe')
 
 # Set a key-value pair
 r.set('my_key', 'Hello, Redis!')
