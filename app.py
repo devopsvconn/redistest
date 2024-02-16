f6650df1451bce8850fbf4dddc3496240e07f92b
Muhammad Farjad <Muhammad.Farjad@SHMA.COM>
2024-02-16 14:47:42 +0500

    redis password added

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import redis
+import base64
+
+redis_password_secret = "redis"  # Replace with the name of your secret
+namespace = "dmsng"
+
+secret_data = kubectl.get_secret(redis_password_secret, namespace)
+redis_password = base64.b64decode(secret_data["data"]["redis-password"]).decode("utf-8")
 
 # Connect to Redis
-r = redis.Redis(host='redis-headless.dmsng.svc.cluster.local', port=6379)
+r = redis.Redis(host='redis-headless.dmsng.svc.cluster.local', port=6379, password=redis_password)
 
 # Set a key-value pair
 r.set('my_key', 'Hello, Redis!')
