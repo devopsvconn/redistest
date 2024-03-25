ace778e3a4818532b5394969114c62144d03ab99
ngAutomationGitOps <ng.automation@vconn.biz>
2024-03-25 17:03:45 +0500

    testing 3:

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import redis
+import os
+
+# Read password from secret
+with open('/etc/redis-password/password', 'r') as f:
+    redis_password = f.read().strip()
 
 # Connect to Redis
-r = redis.Redis(host='bsg-dm01-q-930672-unilevercom-redis-02.privatelink.redis.cache.windows.net', port=6379, password='V2g2sqxeXeYPMlVMDjz3UmvjCy86n8KYMAzCaDq2ZAk=')
+r = redis.Redis(host='bsg-dm01-q-930672-unilevercom-redis-02.privatelink.redis.cache.windows.net', port=6379, password=redis_password)
 
 # Set a key-value pair
 r.set('my_key', 'Hello, Redis!')
