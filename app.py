b6cab45ae4b9c1736b20eefa9c98762781cefd40
ngAutomationGitOps <ng.automation@vconn.biz>
2024-03-25 16:23:32 +0500

    changed some files for azure redis cache connectivity test in ngqa environment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import redis
 
 # Connect to Redis
-r = redis.Redis(host='redis-master.dmsng.svc.cluster.local', port=6379, password='UCFl1FUSGe')
+r = redis.Redis(host='bsg-dm01-q-930672-unilevercom-redis-02.privatelink.redis.cache.windows.net', port=6379, password='V2g2sqxeXeYPMlVMDjz3UmvjCy86n8KYMAzCaDq2ZAk=')
 
 # Set a key-value pair
 r.set('my_key', 'Hello, Redis!')
